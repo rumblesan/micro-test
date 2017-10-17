e8a85df2a86b5ae72ce8218409f053e93e2e5006
Guy John <guy@rumblesan.com>
2017-10-17 16:49:09 +0100

    Make it possible to render a portion of an image

--- haskapi/app/Renderer.hs
+++ haskapi/app/Renderer.hs
@@ -13,10 +13,21 @@ renderImage :: Pixel px => (Color -> px) -> Scene -> Image px
 renderImage pixelRenderer scene@(Scene _ _ _ config) =
   let width = sceneWidth config
       height = sceneHeight config
-  in generateImage
-       (\x y -> pixelRenderer $ getCoordColor scene x (height - y))
-       width
-       height
+  in renderImagePortion pixelRenderer scene (0, 0) (width, height)
+
+{--
+  portion coords are given in pixels
+--}
+renderImagePortion ::
+     Pixel px => (Color -> px) -> Scene -> (Int, Int) -> (Int, Int) -> Image px
+renderImagePortion pixelRenderer scene@(Scene _ _ _ config) (x1, y1) (x2, y2) =
+  let imgWidth = x2 - x1
+      imgHeight = y2 - y1
+  in generateImage coordColor imgWidth imgHeight
+  where
+    coordColor x y =
+      pixelRenderer $
+      getCoordColor scene (x + x1) (sceneHeight config - (y + y1))
 
 pixelRGB8 :: Color -> PixelRGB8
 pixelRGB8 (Color r g b) =
